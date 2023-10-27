195fc7c5a5092cf57ea88ddb5558034ca97c519f
IlyaBokovv <ilyusha215@gmail.com>
2023-10-27 18:50:17 +0300

    Controllers implementation

---
+++ TimeTracking.API/Controllers/ReportController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TimeTracking.Data.DTOs;
+using TimeTracking.Services.Interfaces;
+
+namespace TimeTracking.API.Controllers
+{
+    [Route("api/users/{userId}/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        private readonly IServiceManager _service;
+        public ReportController(IServiceManager serviceManager)
+        {
+            _service = serviceManager;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetReportsForUser(Guid userId)
+        {
+            var reports = await _service.ReportService.GetReportsAsync(userId, false);
+            return Ok(reports);
+        }
+        [HttpGet("{id:guid}", Name = "GetReportForUser")]
+        public async Task<IActionResult> GetReportForUser(Guid userId, Guid id)
+        {
+            var report = await _service.ReportService.GetReportAsync(userId, id, false);
+            return Ok(report);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateReportForUser
+            (Guid userId, [FromBody] ReportCreateAndUpdateDTO report)
+        {
+            var reportToReturn = await _service.ReportService.CreateReportForUserAsync(userId, report,
+                trackChanges: false);
+
+            return CreatedAtAction(nameof(GetReportForUser), new { userId, id = reportToReturn.Id },
+                reportToReturn);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> DeleteReportForUser(Guid userId, Guid id)
+        {
+            await _service.ReportService.DeleteReportForUserAsync(userId, id, false);
+
+            return NoContent();
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> UpdateReportForUser(Guid userId, Guid id,
+            [FromBody] ReportCreateAndUpdateDTO report)
+        {
+            await _service.ReportService.UpdateReportForUserAsync(userId, id, report, true);
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ TimeTracking.API/Controllers/UsersController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TimeTracking.Data.DTOs;
+using TimeTracking.Services.Interfaces;
+
+namespace TimeTracking.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IServiceManager _service;
+
+        public UsersController(IServiceManager service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _service.UserService.GetAllUsersAsync(false);
+
+            return Ok(users);
+        }
+        [HttpGet("{id:guid}", Name = "UserById")]
+        public async Task<IActionResult> GetUser(Guid id)
+        {
+            var user = await _service.UserService.GetUserAsync(id, false);
+            return Ok(user);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] UserCreateAndUpdateDTO user)
+        {
+            var createdUser = await _service.UserService.CreateUserAsync(user);
+
+            return CreatedAtRoute("UserById", new { id = createdUser.Id }, user);
+        }
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> DeleteUser(Guid id)
+        {
+            await _service.UserService.DeleteUserAsync(id, false);
+
+            return NoContent();
+        }
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> UpdateUser(Guid id, [FromBody] UserCreateAndUpdateDTO user)
+        {
+            await _service.UserService.UpdateUserAsync(id, user, true);
+
+            return NoContent();
+        }
+    }
+}
