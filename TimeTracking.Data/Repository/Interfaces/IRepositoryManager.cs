dd28237e870d5104694a702d4784c64566357ccf
IlyaBokovv <ilyusha215@gmail.com>
2023-10-27 18:44:49 +0300

    Repositories

--- TimeTracking.Data/Repository/Interfaces/IRepositoryManager.cs
+++ TimeTracking.Data/Repository/Interfaces/IRepositoryManager.cs
@@ -1,9 +1,9 @@
 ﻿namespace TimeTracking.Data.Repository.Interfaces
 {
-    internal interface IRepositoryManager
+    public interface IRepositoryManager
     {
         IUserRepository User { get; }
         IReportRepository Report { get; }
-        Task SaveAsync();
+        Task SaveChangesAsync();
     }
 }
--- TimeTracking.Data/Repository/Interfaces/IUserRepository.cs
+++ TimeTracking.Data/Repository/Interfaces/IUserRepository.cs
@@ -7,7 +7,6 @@ namespace TimeTracking.Data.Repository.Interfaces
         Task<IEnumerable<User>> GetAllUsersAsync(bool trackChanges);
         Task<User> GetUserAsync(Guid userId, bool trackChanges);
         void CreateUser(User user);
-        Task<IEnumerable<User>> GetByIdsAsync(IEnumerable<Guid> ids, bool trackChanges);
         void DeleteUser(User user);
     }
 }
