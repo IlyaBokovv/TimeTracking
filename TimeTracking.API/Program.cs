2360b645ec2cc129ae8ed789856206b14824672d
IlyaBokovv <ilyusha215@gmail.com>
2023-10-27 18:49:09 +0300

    DI and migrations

--- TimeTracking.API/Program.cs
+++ TimeTracking.API/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using TimeTracking.Data;
+using TimeTracking.Data.Repository;
+using TimeTracking.Data.Repository.Interfaces;
+using TimeTracking.Services;
+using TimeTracking.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +15,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(o => o.UseNpgsql(
     builder.Configuration.GetConnectionString("DbConnection")));
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IReportRepository, ReportRepository>();
+builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
+builder.Services.AddScoped<IServiceManager, ServiceManager>();
 
 var app = builder.Build();
 
---
+++ TimeTracking.Data/Migrations/20231027151755_DateFix.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TimeTracking.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DateFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CurrentDate",
+                table: "Reports",
+                newName: "Date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Reports",
+                newName: "CurrentDate");
+        }
+    }
+}
